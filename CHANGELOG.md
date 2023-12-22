92dc4a8c0d84e2885851ef3ef8ac6d27ee80dff5
Kuloud <kuloud@outlook.com>
2023-12-22 19:56:36 +0800

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+
+## 1.0.0+1 - 2023-12-22.
+* fork from https://pub.dev/packages/amap_flutter_map
+
+
---
+++ lib/amap_base.dart
@@ -0,0 +1,12 @@
+library amap_base;
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+part 'src/amap_api_key.dart';
+part 'src/amap_tools.dart';
+part 'src/callbacks.dart';
+part 'src/location.dart';
+part 'src/poi.dart';
+part 'src/amap_utils.dart';
+part 'src/amap_privacy_statement.dart';
